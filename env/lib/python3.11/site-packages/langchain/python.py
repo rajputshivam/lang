642df0fa01192c15664c49e41fd9479640c61511
rajputshivam <kumarshivam862@gmail.com>
2024-04-14 21:14:02 +0530

    first commit

---
+++ env/lib/python3.11/site-packages/langchain/python.py
@@ -0,0 +1,5 @@
+"""For backwards compatibility."""
+
+from langchain_community.utilities.python import PythonREPL
+
+__all__ = ["PythonREPL"]
---
+++ env/lib/python3.11/site-packages/langchain/requests.py
@@ -0,0 +1,9 @@
+"""DEPRECATED: Kept for backwards compatibility."""
+
+from langchain_community.utilities import Requests, RequestsWrapper, TextRequestsWrapper
+
+__all__ = [
+    "Requests",
+    "RequestsWrapper",
+    "TextRequestsWrapper",
+]
